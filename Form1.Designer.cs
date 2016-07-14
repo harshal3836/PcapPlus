cfbe3857a520941a0355f75c770e9703fa885418
harshal3836 <patelharshal126@gmail.com>
2016-07-14 18:53:50 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace PcapPlus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 92);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(468, 277);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(468, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "%userprofile%\\Desktop";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "*.net",
+            "*.rec",
+            "*.tmp",
+            "*.bin"});
+            this.comboBox1.Location = new System.Drawing.Point(72, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(91, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "*.net";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Filter Files";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(167, 375);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Convert to pcap";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(13, 406);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(98, 13);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Press Select Folder";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 375);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Browse for Wireshark";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(331, 375);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(149, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Merge";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 432);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "PCAP PLUS - Home";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace PcapPlus
+{
+    public partial class Form1 : Form
+    {
+          string[] files = new string[0];
+          string wpath = "";
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            
+            if (result == DialogResult.OK)
+            {
+                //
+                // The user selected a folder and pressed the OK button.
+                // We print the number of files found.
+                //
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+                files = Directory.GetFiles(folderBrowserDialog1.SelectedPath, comboBox1.SelectedItem.ToString());
+                //foreach file as string in files
+                //{
+                listBox1.Items.AddRange(files);
+                //}
+                lblStatus.Text = String.Format("{0} {1} files ready to be joined. Press Convert to pcap above...", files.Length, comboBox1.SelectedItem);
+            }
+            else if (result == DialogResult.Cancel)
+            {
+                myfunction();    
+            }
+            if (files.Length > 0)
+            {
+                this.button2.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myfunction();
+        }
+        private void myfunction()
+        {
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            files = new string[0];
+            listBox1.Items.Clear();
+            this.button2.Enabled = false;
+            lblStatus.Text = "Please browse and select a folder";
+            textBox1.Text = "No folder selected";
+            button3.Enabled = false;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            myfunction();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // "C:\Program Files\Wireshark\editcap.exe"
+            // "C:\Program Files(x86)\Wireshark\editcap.exe"
+            if(File.Exists("C:\\Program Files\\Wireshark\\editcap.exe"))
+            {
+                wpath = "C:\\Program Files\\Wireshark";
+                convert(wpath);
+            }
+            else if (File.Exists("C:\\Program Files(x86)\\Wireshark\\editcap.exe"))
+            {
+                wpath = "C:\\Program Files(x86)\\Wireshark";
+                convert(wpath);
+            }
+            else
+            {
+                lblStatus.Text = "Wireshark not found to be installed. Please browse for wireshark";
+                button3.Enabled = true;
+                browseforwireshark();
+            }
+
+        }
+        private void browseforwireshark()
+        {
+            DialogResult result = folderBrowserDialog2.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                if (File.Exists(folderBrowserDialog2.SelectedPath + "\\editcap.exe"))
+                {
+                    wpath = folderBrowserDialog2.SelectedPath;
+                    convert(wpath);
+                }
+                else
+                {
+                    lblStatus.Text = "Improper folder selected. Please select Wireshark installation folder";
+                }
+            }
+        }
+        private void convert(string path)
+        {
+            try
+            {
+                foreach (string file in files)
+                {
+                    Process.Start(path + "\\editcap.exe", String.Format("{0} {1}", file, file.Replace(comboBox1.SelectedItem.ToString().Replace("*", ""), ".pcap")));
+                    File.AppendAllText("PcapPlus.log", String.Format("{0}\\editcap.exe {1}", path, String.Format("{0} {1}", file, file.Replace(comboBox1.SelectedItem.ToString().Replace("*",""), ".pcap"))));
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Exception: " + ex.Message.ToString());
+                
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process.Start(wpath + "\\mergecap.exe", "-F pcap -w merged.pcap " + folderBrowserDialog1.SelectedPath + "\\*.pcap");
+        }
+
+
+     
+
+     
+
+       
+    }
+}
